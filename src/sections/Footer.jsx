e0152b5b1ecaf9c5d61173faa02f74e9ebf79d0a
Omkar Bodhe <omkarbodhe3@gmail.com>
2025-08-16 12:46:45 +0530

    Icon Link Added

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,13 +8,32 @@ const Footer = () => {
         <div className="flex flex-col justify-center">
           <p>Terms & Conditions</p>
         </div>
+
         <div className="socials">
           {socialImgs.map((socialImg, index) => (
             <div key={index} className="icon">
-              <img src={socialImg.imgPath} alt="social icon" />
+              <img
+                src={socialImg.imgPath}
+                alt="social icon"
+                onClick={() => {
+                  if (socialImg.name === "insta") {
+                    window.open(
+                      "https://www.instagram.com/er_omii_1923?igsh=M3JudTQ5OTYzZTM5",
+                      "_blank"
+                    );
+                  } else if (socialImg.name === "linkedin") {
+                    window.open(
+                      "https://www.linkedin.com/in/omkar-bodhe-aab633227",
+                      "_blank"
+                    );
+                  }
+                }}
+                style={{ cursor: "pointer" }}
+              />
             </div>
           ))}
         </div>
+
         <div className="flex flex-col justify-center">
           <p className="text-center md:text-end">
             © {new Date().getFullYear()} Omkar Bodhe. All rights reserved.
