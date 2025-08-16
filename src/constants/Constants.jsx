fd52d9f0abc4b42dcd69d5171189fa08ad752cde
Omkar Bodhe <omkarbodhe3@gmail.com>
2025-08-16 12:27:43 +0530

    icon added

--- src/constants/Constants.jsx
+++ src/constants/Constants.jsx
@@ -155,14 +155,7 @@ const socialImgs = [
     name: "insta",
     imgPath: "/images/insta.png",
   },
-  {
-    name: "fb",
-    imgPath: "/images/fb.png",
-  },
-  {
-    name: "x",
-    imgPath: "/images/x.png",
-  },
+
   {
     name: "linkedin",
     imgPath: "/images/linkedin.png",
